f4ec7a55220bfcc501af37ebbbc2c3b18f87b62e
YaroslavAntoniuk <i23-1912-701-01@lute.lviv.ua>
2024-03-02 15:46:48 +0200

    Updated user profile page

--- app/(dashboard)/profile/page.js
+++ app/(dashboard)/profile/page.js
@@ -1,5 +1,11 @@
+import { UserProfile } from '@clerk/nextjs';
+
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="p-8">
+      <UserProfile />
+    </div>
+  );
 };
 
 export default Profile;
