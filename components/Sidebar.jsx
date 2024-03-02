84783f10ffad5f7a884210be766360befb5869b6
YaroslavAntoniuk <i23-1912-701-01@lute.lviv.ua>
2024-03-02 15:37:09 +0200

    Added theme logic

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -4,7 +4,7 @@ import SidebarHeader from './SidebarHeader';
 
 const Sidebar = () => {
   return (
-    <div className="px-4 w-80 min-h-full bg-slate-800 py-12 grid grid-rows-[auto_1fr_auto]">
+    <div className="px-4 w-80 min-h-full bg-base-300 py-12 grid grid-rows-[auto_1fr_auto]">
       <SidebarHeader />
       <NavLinks />
       <MemberProfile />
--- components/ThemeToggle.jsx
+++ components/ThemeToggle.jsx
@@ -1,5 +1,31 @@
+'use client';
+import { useState } from 'react';
+import { BsMoonFill, BsSunFill } from 'react-icons/bs';
+
+const themes = {
+  lofi: 'lofi',
+  luxury: 'luxury',
+};
+
 const ThemeToggle = () => {
-  return <button className="btn btn-primary btn-sm">ThemeToggle</button>;
+  const [theme, setTheme] = useState(themes.luxury);
+
+  const toggleTheme = () => {
+    const newTheme = theme === themes.luxury ? themes.lofi : themes.luxury;
+
+    document.documentElement.setAttribute('data-theme', newTheme);
+    setTheme(newTheme);
+  };
+
+  return (
+    <button onClick={toggleTheme} className="btn btn-sm btn-circle btn-outline">
+      {theme === themes.lofi ? (
+        <BsMoonFill className="h-4 w-4" />
+      ) : (
+        <BsSunFill className="h-4 w-4" />
+      )}
+    </button>
+  );
 };
 
 export default ThemeToggle;
