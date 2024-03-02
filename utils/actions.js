8c920fc56d6f83db6e24deff05ecbf5cdb23e32f
YaroslavAntoniuk <i23-1912-701-01@lute.lviv.ua>
2024-03-02 16:59:04 +0200

    Added initial OpenAI setup

--- utils/actions.js
+++ utils/actions.js
@@ -1,6 +1,24 @@
 'use server';
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 export const generateChatResponse = async (message) => {
-  console.log(message);
+  const response = await openai.chat.completions.create({
+    messages: [
+      {
+        role: 'system',
+        content:
+          'You are a helpful assistant that can provide any info about the cities and countries.',
+      },
+      { role: 'user', content: message },
+    ],
+    model: 'gpt-3.5-turbo',
+    temperature: 0,
+  });
+  console.log(response.choices[0].message);
+
   return 'Hello! How can I help you today?';
 };
