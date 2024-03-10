36775db8fcf4f269a5ef40945f163f0b3cf023ac
YaroslavAntoniuk <i23-1912-701-01@lute.lviv.ua>
2024-03-10 18:52:02 +0200

    Updated generateTourResponse to use a prompt and return a result

--- utils/actions.js
+++ utils/actions.js
@@ -1,5 +1,6 @@
 'use server';
 import OpenAI from 'openai';
+import { createTourPrompt } from './prompts';
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -32,7 +33,27 @@ export const getExistingTour = async ({ city, country }) => {
 };
 
 export const generateTourResponse = async ({ city, country }) => {
-  return null;
+  try {
+    const response = await openai.chat.completions.create({
+      messages: [
+        { role: 'system', content: 'You are a tour guide' },
+        { role: 'user', content: createTourPrompt({ city, country }) },
+      ],
+      model: 'gpt-3.5-turbo',
+      temperature: 0,
+    });
+
+    const tourData = JSON.parse(response.choices[0].message.content);
+
+    if (!tourData.tour) {
+      return null;
+    }
+
+    return tourData.tour;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 };
 
 export const createNewTour = async (tour) => {
