c46bfb0f955eaea609c146cac3d9574012b23ff1
YaroslavAntoniuk <i23-1912-701-01@lute.lviv.ua>
2024-03-10 19:45:04 +0200

    Updated tour info component

--- components/TourInfo.jsx
+++ components/TourInfo.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import Stops from './Stops';
 
-const TourInfo = () => {
-  return <div>TourInfo</div>;
+const TourInfo = ({ tour }) => {
+  const { city, country, title, description, stops, suggestedPlaces } = tour;
+  return (
+    <div className="max-w-2xl">
+      <h2 className="mb-4">
+        <span className="text-2xl font-semibold">{title}</span>
+        <span className="text-gray-500 ml-2">
+          {city}, {country}
+        </span>
+      </h2>
+      <p>{description}</p>
+      <Stops stops={stops} />
+      <h3 className="mt-8 mb-4 text-2xl font-semibold">Suggested places</h3>
+      <ul>
+        {suggestedPlaces.map((place, index) => (
+          <li key={index}>{place}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default TourInfo;
