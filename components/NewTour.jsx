508270905f0777893e1f69f0c33a0ccd121df83b
YaroslavAntoniuk <i23-1912-701-01@lute.lviv.ua>
2024-03-17 17:35:01 +0200

    Update mutation function for creating a new tour

--- components/NewTour.jsx
+++ components/NewTour.jsx
@@ -1,18 +1,32 @@
 'use client';
-import { generateTourResponse } from '@/utils/actions';
-import { useMutation } from '@tanstack/react-query';
+import {
+  createNewTour,
+  generateTourResponse,
+  getExistingTour,
+} from '@/utils/actions';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
 import TourInfo from './TourInfo';
 
 const NewTour = () => {
+  const queryClient = useQueryClient();
   const {
     mutate,
     isPending,
     data: tour,
   } = useMutation({
     mutationFn: async (destination) => {
+      const existingTour = await getExistingTour(destination);
+
+      if (existingTour) {
+        return existingTour;
+      }
+
       const newTour = await generateTourResponse(destination);
       if (newTour) {
+        await createNewTour(newTour);
+        queryClient.invalidateQueries({ queryKey: ['tours'] });
+
         return newTour;
       }
       toast.error(
