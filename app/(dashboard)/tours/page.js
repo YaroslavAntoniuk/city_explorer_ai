8b8514e18f713c58a000f999a2938bd7e2a7f6af
YaroslavAntoniuk <i23-1912-701-01@lute.lviv.ua>
2024-03-17 17:50:53 +0200

    Updated tours page with query client & prefetch functionality

--- app/(dashboard)/tours/page.js
+++ app/(dashboard)/tours/page.js
@@ -1,5 +1,24 @@
-const Tours = () => {
-  return <div>Tours</div>;
+import ToursList from '@/components/ToursList';
+import { getAllTours } from '@/utils/actions';
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from '@tanstack/react-query';
+
+const Tours = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['tours'],
+    queryFn: () => getAllTours(),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ToursList />
+    </HydrationBoundary>
+  );
 };
 
 export default Tours;
